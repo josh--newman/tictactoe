8853c510b2e518e7813e017ec26e2f8dd4ccfe13
Josh Newman <josh.newman4390@gmail.com>
2016-03-26 14:32:44 -0700

    Refine socket testing

--- server/socketevents.js
+++ server/socketevents.js
@@ -1,5 +1,15 @@
 exports = module.exports = function(io) {
-  io.on('connection', function(client) {
-    console.log('Client connected with ID:', client.id);
+  io.on('connection', function(socket) {
+    console.log('Client connected with ID:', socket.id);
+
+    socket.on('disconnect', function() {
+      console.log('Client disconnected with ID:', socket.id);
+      // kick player out of game
+    });
+
+    socket.on('join queue', function(data) {
+      console.log('Player joined queue', socket.id, data.name);
+      socket.emit('join queue', "Joined");
+    });
   });
 };
--- server/test/socket_test.js
+++ server/test/socket_test.js
@@ -4,16 +4,35 @@ import io from 'socket.io-client';
 const port = process.env.PORT || 3090;
 const socketURL = 'http://localhost:' + port;
 
-const options = {
-  transports: ['websocket'],
-  'force new connection': true
-};
-
-const player1 = { 'name': 'Josh' };
-const player2 = { 'name': 'Bill' };
-
 describe('Socket communication', () => {
-  it('Should do something on connection', () => {
+  let server;
+
+  const player1 = { 'name': 'Josh' };
+  const player2 = { 'name': 'Bill' };
+  const options = {
+    transports: ['websocket'],
+    'force new connection': true
+  };
+
+  beforeEach((done) => {
+    // start server
+    server = require('../index.js').server;
+    done();
+  });
+
+  it('registers a new player', (done) => {
     const client = io.connect(socketURL, options);
+
+    client.once('connect', (data) => {
+      client.once('join queue', (message) => {
+        expect(message).to.equal('Joined');
+
+        client.disconnect();
+        done();
+      });
+
+      client.emit('join queue', player1);
+    });
+
   });
 });
