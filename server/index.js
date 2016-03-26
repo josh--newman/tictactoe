39158ebc5b9d0dd15282731802f1e970ea6d185e
Josh Newman <josh.newman4390@gmail.com>
2016-03-26 11:19:46 -0700

    Set up server and index.html

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const path = require('path');
 
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+const port = process.env.PORT || 3090;
+server.listen(port);
+console.log('Server listening on port:', port);
