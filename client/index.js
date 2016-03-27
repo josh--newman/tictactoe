d8fd5b9a3c9480e77d1fea9bc26b91949b7f3d77
Josh Newman <josh.newman4390@gmail.com>
2016-03-27 12:51:34 -0700

    Remove redux stuff

--- client/index.js
+++ client/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+
 import styles from './styles/main.scss';
-
 import App from './src/components/App';
-import reducers from './src/reducers';
 
-const createStoreWithMiddleWare = applyMiddleware()(createStore);
-
-render(
-  <Provider store={createStoreWithMiddleWare(reducers)}>
-    <App />
-  </Provider> , document.querySelector('.container'));
+render(<App />, document.querySelector('.container'));
